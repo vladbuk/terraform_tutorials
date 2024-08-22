3850d60ca699448bb832d2dccf5fcec2dc19af9f
vladbuk <vladbuk@gmail.com>
2024-08-22 11:48:17 +0300

    outputs

--- learn-terraform-gcp/main.tf
+++ learn-terraform-gcp/main.tf
@@ -34,3 +34,11 @@ resource "google_compute_instance" "vm_instance" {
     }
   }
 }
+
+output "ip" {
+  value = google_compute_instance.vm_instance.network_interface.0.network_ip
+}
+
+output "public_ip" {
+  value = google_compute_instance.vm_instance.network_interface.0.access_config[0].nat_ip
+}
