4f783e6d8b875e076c42ab2f2444ca8cb1e97edd
vladbuk <vladbuk@gmail.com>
2024-08-22 10:43:04 +0300

    f1-micro added

--- learn-terraform-gcp/main.tf
+++ learn-terraform-gcp/main.tf
@@ -14,3 +14,22 @@ provider "google" {
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  zone         = "us-east1-b"
+  tags         = ["web", "dev"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
