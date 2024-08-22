ab0b312e331bce025443563328b36f9af81454ef
vladbuk <vladbuk@gmail.com>
2024-08-22 11:33:13 +0300

    cos-stable

--- learn-terraform-gcp/main.tf
+++ learn-terraform-gcp/main.tf
@@ -23,7 +23,7 @@ resource "google_compute_instance" "vm_instance" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
+      image = "cos-cloud/cos-stable"
     }
   }
 
